5c09aaa847452c4b8bd2f8364e5d9c15a0e0953f
IgnaceMenace <igparmentier@gmail.com>
2021-11-18 01:22:37 +0100

    TRying to set input and interact with the add on

--- orange/clockwork/widgets/__init__.py
+++ orange/clockwork/widgets/__init__.py
@@ -1,1 +1,4 @@
+import sysconfig
 
+ICON = "icons/category.svg"
+#can still add a background color for the category and a path to help
--- orange/clockwork/widgets/widget1.py
+++ orange/clockwork/widgets/widget1.py
@@ -1,18 +1,17 @@
-from AnyQt.QtWidgets import QLabel
-from Orange.widgets.widget import OWWidget
+from Orange.widgets.widget import OWWidget, Output
+from Orange.widgets import gui
+from Orange.widgets import settings
 
-class MyWidget(OWWidget):
+class widget1(OWWidget):
     name = "First Widget"
     icon = "icons/widget1.svg"
-    
-    
+    description = "First widget, input something and return something else"    
+    number = 2
     def __init__(self):
         super().__init__()
-
-        label = QLabel("Ca marche ?")
-        self.controlArea.layout().addWidget(label)
-
+        a = float(input("entrer le nombre : "))
+        gui.widgetLabel(self.controlArea, "Ca marche ? " + str(a*2))
 
 if __name__ == "__main__":
     from Orange.widgets.utils.widgetpreview import WidgetPreview  # since Orange 3.20.0
-    WidgetPreview(MyWidget).run()
+    WidgetPreview(widget1).run()
