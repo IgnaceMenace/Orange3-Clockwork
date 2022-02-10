deaabfc37ef52a14e9ca6b5fd121aa4345e53bbd
SireMoulax <94380317+SireMoulax@users.noreply.github.com>
2022-02-10 12:52:17 +0100

    Ah ben oui

--- FFTtest.py
+++ FFTtest.py
@@ -1,7 +1,7 @@
 import numpy
 from scipy.fftpack import fft
 from scipy import signal
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 xArray = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
 yArray = numpy.array([15, 25, 35, 45, 55, 65, 75, 85, 95, 105])
@@ -13,6 +13,7 @@ def FFTG(xInput, yInput):
     start = 0.0
     stop = 1.0/(2.0*period)
     num = int(xLength/2)
+    
     xSpect = numpy.linspace(start, stop, num, dtype = float)
     print("xSpect is : " , xSpect)
 
@@ -22,8 +23,8 @@ def FFTG(xInput, yInput):
     ySpect = 2.0/xLength * numpy.abs(ywf[0:int(xLength/2)])
     print("ySpect is : " , ySpect)
 
-    matplotlib.pyplot.plot(xSpect,ySpect)
-    matplotlib.pyplot.show()
+    plt.plot(xSpect,ySpect)
+    plt.show()
 
 #FFTG(xArray, yArray)
 
@@ -32,9 +33,9 @@ amplitude = 2*numpy.sin(time)
 
 amplitude1 = numpy.cos(2*time)
 FFTG(time, amplitude)
-FFTG(time, amplitude1)
-print("time", time)
-FFTG(time, amplitude + amplitude1)
+#FFTG(time, amplitude1)
+#print("time", time)
+#FFTG(time, amplitude + amplitude1)
 
 def FFTV(xInput,yInput):
     print("FFTV")
