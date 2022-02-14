5f80c2816fbdec3ad7a384224d13ac898f9c07f1
IgnaceMenace <igparmentier@gmail.com>
2022-02-14 17:29:15 +0100

    First documentation comments added

--- FFTtest.py
+++ FFTtest.py
@@ -1,12 +1,28 @@
 import numpy
 from scipy.fftpack import fft
 from scipy import signal
-import matplotlib.pyplot as plt
+import matplotlib.pyplot
 
-xArray = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-yArray = numpy.array([15, 25, 35, 45, 55, 65, 75, 85, 95, 105])
 
 def FFTG(xInput, yInput):
+    """
+    This function calculate the Frequential Fourier Transform in Acceleration 
+
+    Parameters
+    ----------
+    xInput : array
+        Array of a certain size that represent the temporal window
+    yInput : array
+        Array filled with the temporal response associated with every values of the xInput
+
+    Returns
+    -------
+    xSpectral : array
+        Array filled with the frequential window
+    ySpectral : array
+        Array filled with the amplitude associated with each frequency 
+    """
+
     xLength = int(len(xInput)) #determine length of the array
     period = numpy.diff(xInput).mean() #determine period by the average of each element minus the one before
     
@@ -14,45 +30,74 @@ def FFTG(xInput, yInput):
     stop = 1.0/(2.0*period)
     num = int(xLength/2)
     
-    xSpect = numpy.linspace(start, stop, num, dtype = float)
-    print("xSpect is : " , xSpect)
+    xSpectral = numpy.linspace(start, stop, num, dtype = float)
+    print("xSpectral is : " , xSpectral)
 
     #filtering = signal.boxcar(xLength)
     #ywf = fft(yInput*filtering)
     ywf = fft(yInput)
-    ySpect = 2.0/xLength * numpy.abs(ywf[0:int(xLength/2)])
-    print("ySpect is : " , ySpect)
+    ySpectral = 2.0/xLength * numpy.abs(ywf[0:int(xLength/2)])
+    print("ySpectral is : " , ySpectral)
 
-    plt.plot(xSpect,ySpect)
-    plt.show()
+
+    #matplotlib.pyplot.plot(xSpectral,ySpectral)
+    #matplotlib.pyplot.show()
+
+    return [xSpectral , ySpectral]
 
 def FFTV(xInput,yInput):
-    period = numpy.diff(xInput).mean() #determine period by the average of each element minus the one before
-    xLength = int(len(xInput)) #determine length of the array
+    """
+    This function calculate the Frequential Fourier Transform in Velocity 
+
+    Parameters
+    ----------
+    xInput : array
+        Array of a certain size that represent the temporal window
+    yInput : array
+        Array filled with the temporal response associated with every values of the xInput
+
+    Returns
+    -------
+    xSpectral : array
+        Array filled with the frequential window
+    ySpectral : array
+        Array filled with the amplitude associated with each frequency 
+    """
+    xLength = int(len(xInput))
+    period = numpy.diff(xInput).mean()
+    
     start = 0.0
     stop = 1.0/(2.0*period)
     num = int(xLength/2)
-    xSpect = numpy.linspace(start, stop, num, dtype=float)
-    #filtering = signal.boxcar(xLength)
-    #ywf = fft(yInput*filtering)
-    ywf = fft(yInput)
-    ySpect = int((9810 * (numpy.sqrt(2)/2))/int(xLength) *numpy.pi * numpy.abs(ywf[0:int(xLength)/2]))
+
+    xSpectral = numpy.linspace(0.0, 1.0/(2.0*period), num, dtype=float)
+    ywm = fft(yInput)
+    ySpectral = (9810 * (numpy.sqrt(2)/2))/(2*num*numpy.pi) * numpy.abs(ywm[0:num])
     
-    xSpect[0] = 0
-    ySpect = numpy.divide(ySpect[1:],xSpect[1:])
-    ySpect = numpy.concatenate(([0],ySpect))
-    
-    matplotlib.pyplot.plot(xSpect,ySpect)
-    matplotlib.pyplot.show()
+    xSpectral[0] = 0
+    ySpectral = numpy.divide(ySpectral[1:],xSpectral[1:])
+    ySpectral = numpy.concatenate(([0],ySpectral))
+
+    #matplotlib.pyplot.plot(xSpectral,ySpectral)
+    #matplotlib.pyplot.show()
+    return [xSpectral , ySpectral]
 
 if __name__ == "__main__":
-    
     time = numpy.arange(0,100,0.1)
     amplitude = 2*numpy.sin(time)
     amplitude1 = numpy.cos(2*time)
-    #FFTG(xArray, yArray)
-    #FFTG(time, amplitude)
-    #FFTG(time, amplitude1)
-    #print("time", time)
-    FFTG(time, amplitude + amplitude1)
-    FFTV(time, amplitude + amplitude1)
+
+    [xSpectralFFTG,ySpectralFFTG] = FFTG(time, amplitude + amplitude1)
+    [xSpectralFFTV,ySpectralFFTV] = FFTV(time, amplitude + amplitude1)
+
+    # Initialise the subplot function using number of rows and columns
+    figure, axis = matplotlib.pyplot.subplots(1,2)
+    axis[0].plot(xSpectralFFTG,ySpectralFFTG)
+    axis[0].set_title("FFTG")
+    axis[0].set_xlabel ('Fréquences (Hz)')
+    axis[0].set_ylabel ('Amplitude')
+    axis[1].plot(xSpectralFFTV,ySpectralFFTV)
+    axis[1].set_title("FFTV")
+    axis[1].set_xlabel ('Fréquences (Hz)')
+    axis[1].set_ylabel ('Amplitude')
+    matplotlib.pyplot.show()
