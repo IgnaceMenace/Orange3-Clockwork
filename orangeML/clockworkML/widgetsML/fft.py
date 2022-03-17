1c2ee4cfddf79d6629b6781f607585357f64bdaa
IgnaceMenace <igparmentier@gmail.com>
2022-03-17 11:24:59 +0100

    Working on the reception of Orange.data.table processing it with fft and sending it

--- orangeML/clockworkML/widgetsML/fft.py
+++ orangeML/clockworkML/widgetsML/fft.py
@@ -12,6 +12,13 @@ class widgetFFT(OWWidget):
     name = "FFT Widget"
     icon = "icons/widget1.svg"
     description = "Widget that calculate the FFTs out vibration data"    
+    class Inputs:
+        inputWidget2 = Input("Second input", Table)
+    class Outputs:
+        ouputWidget2 = Output("Second output", Table)
+    class error(widget.OWWidget.Error):
+        defaultError = Msg("Error while treating data")
+    want_main_area = False
     def __init__(self):
         class FFTs:
             def FFTG(self,xInput, yInput):
@@ -102,6 +109,19 @@ class widgetFFT(OWWidget):
         axis[1].set_xlabel ('Fréquences (Hz)')
         axis[1].set_ylabel ('Amplitude')
         matplotlib.pyplot.show()
+
+    @Inputs.inputWidget2
+    def set_data(self, dataset):
+        if dataset is not None:
+            print("It works !")
+            print(dataset.shuffle())
+            print("Column ID is : " , dataset.columns)
+            print("Domain is : " , dataset.domain)
+            print(dataset)
+            self.Outputs.ouputWidget2.send(dataset)
+        else:
+            self.infoa.setText('No data on input yet, waiting to get something.')
+            self.Outputs.sample.send("Sampled Data")
 
 if __name__ == "__main__":
     from Orange.widgets.utils.widgetpreview import WidgetPreview  # since Orange 3.20.0
