1466c4a4dd389a323793acf97e9168bf55ffb7f0
IgnaceMenace <igparmentier@gmail.com>
2022-05-05 15:10:27 +0200

    Creation of the basic widget before implementation

--- orangeML/clockworkML/widgetsML/widgetFFTwip.py
+++ orangeML/clockworkML/widgetsML/widgetFFTwip.py
@@ -9,7 +9,7 @@ from scipy.fftpack import fft
 import matplotlib.pyplot
 
 class dataProcessing:
-    def fft(self, xInput, yInput):
+    def FFTG(self, xInput, yInput):
         """
         This function calculate the Frequential Fourier Transform in Acceleration 
 
@@ -36,6 +36,36 @@ class dataProcessing:
         xSpectral = numpy.linspace(start, stop, num, dtype = float)
         ySpectral = 2.0/xLength * numpy.abs(ywm[0:int(xLength/2)])
         return xSpectral, ySpectral
+    def FFTV(self,xInput,yInput):
+        """
+        This function calculate the Frequential Fourier Transform in Velocity 
+
+        Parameters
+        ----------
+        xInput : array
+            Array of a certain size that represent the temporal window
+        yInput : array
+            Array filled with the temporal response associated with every values of the xInput
+
+        Returns
+        -------
+        xSpectral : array
+            Array filled with the frequential window
+        ySpectral : array
+            Array filled with the amplitude associated with each frequency 
+        """
+        xLength = int(len(xInput))
+        period = numpy.diff(xInput).mean()
+        start = 0.0
+        stop = 1.0/(2.0*period)
+        num = int(xLength/2)
+        xSpectral = numpy.linspace(0.0, 1.0/(2.0*period), num, dtype=float)
+        ywm = fft(yInput)
+        ySpectral = (9810 * (numpy.sqrt(2)/2))/(2*num*numpy.pi) * numpy.abs(ywm[0:num])
+        xSpectral[0] = 0
+        ySpectral = numpy.divide(ySpectral[1:],xSpectral[1:])
+        ySpectral = numpy.concatenate(([0],ySpectral))
+        return xSpectral , ySpectral
     def dataTableBuilder(self, xInput, yInput):
         yInput = numpy.array(yInput)
         xInput = numpy.array(xInput)
@@ -86,16 +116,21 @@ class widgetFFT(OWWidget):
                         curlyBraceFlag = 1
                     formatedDataY.append(float(iFD))
             dP = dataProcessing()
-            [xSpectralFFTG,ySpectralFFTG] = dP.fft(formatedDataX, formatedDataY)
-            figure, axis = matplotlib.pyplot.subplots(1,2)
-            axis[0].plot(xSpectralFFTG,ySpectralFFTG)
-            axis[0].set_title("FFTG")
-            axis[0].set_xlabel ('Fréquences (Hz)')
-            axis[0].set_ylabel ('Amplitude')
-            axis[1].plot(formatedDataX,formatedDataY)
-            axis[1].set_title("Data")
-            axis[1].set_xlabel ('Time [s]')
-            axis[1].set_ylabel ('Amplitude')
+            [xSpectralFFTG,ySpectralFFTG] = dP.FFTG(formatedDataX, formatedDataY)
+            [xSpectralFFTV,ySpectralFFTV] = dP.FFTV(formatedDataX, formatedDataY)
+            figure, axis = matplotlib.pyplot.subplots(2,2)
+            axis[0,0].plot(xSpectralFFTG,ySpectralFFTG)
+            axis[0,0].set_title("FFTG")
+            axis[0,0].set_xlabel ('Fréquences (Hz)')
+            axis[0,0].set_ylabel ('Amplitude')
+            axis[0,1].plot(xSpectralFFTV,ySpectralFFTV)
+            axis[0,1].set_title("FFTV")
+            axis[0,1].set_xlabel ('Ordre')
+            axis[0,1].set_ylabel ('Amplitude')
+            axis[1,0].plot(formatedDataX,formatedDataY)
+            axis[1,0].set_title("Data")
+            axis[1,0].set_xlabel ('Time [s]')
+            axis[1,0].set_ylabel ('Amplitude')
             matplotlib.pyplot.show()
             dataTableOutput = dP.dataTableBuilder(xSpectralFFTG, ySpectralFFTG)
             print(f"Output data.Table {dataTableOutput}")
